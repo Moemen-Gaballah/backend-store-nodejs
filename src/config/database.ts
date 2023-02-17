736fa6d26905cd1a4a060913738b27ea4c4b38bc
Moemen Gaballa <moemengaballa@gmail.com>
2023-02-17 21:21:11 +0200

    test on db test not dev

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,8 +9,7 @@ const {
   POSTGRES_PORT,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
-  POSTGRES_TEST_DB,
-  POSTGRES_TEST_PORT,
+  POSTGRES_PORT_TEST,
   APP_ENV,
 } = process.env;
 
@@ -19,8 +18,8 @@ let client;
 if (APP_ENV === "test") {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
-    port: POSTGRES_TEST_PORT as unknown as number,
+    database: POSTGRES_DB,
+    port: POSTGRES_PORT_TEST as unknown as number,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   });
